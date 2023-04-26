0dfbf9b41a54c8604acac3e84a20311f61c429fd
Keerthi-H <130353519+Keerthi-H@users.noreply.github.com>
2023-04-26 18:05:00 +0530

    Add files via upload

---
+++ keerthiH-adminboot/src/main/java/com/example/adminboot/KeerthiHAdminbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeerthiHAdminbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeerthiHAdminbootApplication.class, args);
+	}
+
+}
